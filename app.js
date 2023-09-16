a6b9a74526a18731dc7e3a7bdeff1bdc75b1b555
Dissonant101 <92066010+Dissonant101@users.noreply.github.com>
2023-09-16 09:10:20 -0400

    Add endpoints and fix telemetry error

--- app.js
+++ app.js
@@ -8,31 +8,52 @@ const Sequelize = require('sequelize-cockroachdb');
 
 const fs = require('fs');
 
-var sequelize = new Sequelize({
-    dialect: "postgres",
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: "plants",
-    dialectOptions: {
-        ssl: {
-
-            //For secure connection:
-            ca: fs.readFileSync('certs/root.crt')
-                .toString()
-        },
-    },
-    logging: false,
-});
+var sequelize = new Sequelize(process.env.DATABASE_URL)
 
 const Plant = sequelize.define("plants", {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true,
+    },
+    timestamp: {
+        type: Sequelize.STRING
+    },
+    lightLevel: {
+        type: Sequelize.DECIMAL
+    },
+    moistureLevel: {
+        type: Sequelize.DECIMAL
     }
 });
+
+app.post('/api/plants', function (req, res) {
+    Plant.sync({
+        force: false,
+    })
+        .then(function () {
+            return Plant.bulkCreate([
+                {
+                    timestamp: req.body.timestamp,
+                    lightLevel: req.body.lightLevel,
+                    moistureLevel: req.body.moistureLevel
+                },
+            ]);
+        })
+
+        .catch(function (err) {
+            console.error("error: " + err.message);
+        });
+})
+
+app.get('/api/plants', (req, res) => {
+    Plant.sync({
+        force: false,
+    })
+        .then(function () {
+            return Plant.findAll();
+        })
+})
 
 app.listen(port, host, () => {
     console.log(`Server started at ${host} port ${port}`);
