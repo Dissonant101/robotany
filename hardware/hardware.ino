34648999b9508126a6f88631cadffdab83dd1ca7
Jay <jay.jrjren@gmail.com>
2023-09-16 23:46:44 -0400

    Send data and check isWatering

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -10,6 +10,7 @@ sure if it's installed by default or not
 */
 
 #include <Arduino.h>
+#include <Arduino_JSON.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
@@ -21,87 +22,104 @@ const int ADC0 = 36;
 const int ADC5 = 33;
 const int GPIO16 = 16;
 const int GPIO17 = 17;
+
+const int pollDelay = 4000;
+
 WiFiMulti wifiMulti;
 
-const int pollDelay = 50;
-
-// 0 = not spinning, 1 = clockwise, 2 = counter-clockwise
-int motorState = 1;
-int timeUntilSwitchMotorState = 5000;
+int previousIsWatering = false;
 
 int previousLightLevel = 0;
-int previousMoisture = 0;
+int previousMoistureLevel = 0;
 
 Servo myServo;
 
 void setup() {
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-    Serial.begin(115200);
-    Serial.println("Connecting to wifi...");
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  Serial.begin(115200);
+  Serial.println("Connecting to wifi...");
 
-    wifiMulti.addAP("HackTheNorth", "HTNX2023");
-    delay(500);
-    Serial.println("Wifi Connected");
-    
-    myServo.attach(GPIO16);
-    myServo.write(0);
+  wifiMulti.addAP("HackTheNorth", "HTNX2023");
+  delay(500);
+  Serial.println("Wifi Connected");
+  
+  myServo.attach(GPIO16);
 }
 
 void updateMotor() {
-  timeUntilSwitchMotorState -= pollDelay;
-  if (timeUntilSwitchMotorState <= 0) {
-    motorState = (motorState + 1) % 2;
-    timeUntilSwitchMotorState = 5000;
-    if (motorState == 0) {
-      Serial.println("Motor state 0");
-      myServo.write(0);
-    } else if (motorState == 1) {
-      Serial.println("Motor state 1");
-      myServo.write(90);
+  HTTPClient http;
+  http.begin("http://ec2-54-242-100-215.compute-1.amazonaws.com/api/plants");
+  
+  int httpCode = http.GET();
+      
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.print("updateMotor: Received response from server");
+    JSONVar data = JSON.parse(payload);
+    boolean isWatering = data[0]["isWatering"];
+    if (previousIsWatering && !isWatering) {
+      Serial.println("updateMotor: Entering watering state");
+      myServo.writeMicroseconds(1500);
+      previousIsWatering = isWatering;
+    } else if (!previousIsWatering && isWatering) {
+      Serial.println("updateMotor: Entering not watering state");
+      myServo.writeMicroseconds(1000);
+      previousIsWatering = isWatering;
+    } else {
+      Serial.println("updateMotor: No changes to watering");
     }
+  } else {
+    Serial.print("updateMotor: Encountered error ");
+    Serial.print(httpCode);
+    Serial.println(http.errorToString(httpCode).c_str());
   }
+  http.end();
 }
 
+void sendData(int lightLevel, int moistureLevel) {
+  HTTPClient http;
+  http.begin("http://ec2-54-242-100-215.compute-1.amazonaws.com/api/plantdata");
+  http.addHeader("Content-Type", "application/json");
+
+  String payload = "{\"name\": ";
+  payload += "\"Jerry\"";
+  payload += ", \"lightLevel\": ";
+  payload += lightLevel;
+  payload += ", \"moistureLevel\": ";
+  payload += moistureLevel;
+  payload += "}";
+  int httpCode = http.POST(payload);
+
+  if(httpCode > 0) {
+    Serial.print("sendData: Posted data successfully");
+  } else {
+    Serial.print("sendData: Encountered error ");
+    Serial.print(httpCode);
+    Serial.println(http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+}
+
+
 void loop() {
+  Serial.print("=== Next loop ===");
+  if ((wifiMulti.run() == WL_CONNECTED)) {
+    int lightLevel = analogRead(ADC0);
+    int moistureLevel = analogRead(ADC5);
+    int smoothedLightLevel = (previousLightLevel + lightLevel) / 2;
+    int smoothedMoistureLevel = (previousMoistureLevel + moistureLevel) / 2;
+    sendData(smoothedLightLevel, smoothedMoistureLevel);
     updateMotor();
-    if ((wifiMulti.run() == WL_CONNECTED)) {
-        int lightLevel = analogRead(ADC0);
-        int moisture = analogRead(ADC5);
-        int smoothedLightLevel = (previousLightLevel + lightLevel) / 2;
-        int smoothedMoisture = (previousMoisture + moisture) / 2;
-        Serial.print("L:");
-        Serial.print(smoothedLightLevel);
-        delay(10);
-        //      Serial.println("");
-        Serial.print(" M:");
-        Serial.println(moisture);
-        previousLightLevel = smoothedLightLevel;
-        previousMoisture = smoothedMoisture;
-        //      HTTPClient http;
-        //      http.begin("https://hackthenorth2022.uc.r.appspot.com/api/velocities");
-        //      http.addHeader("Content-Type", "application/json");
-        //
-        //      String payload = "{\"humidity\": ";
-        //      payload += humidity;
-        //      payload += ", \"light\": ";
-        //      payload += light;
-        //      payload += "}";
-        //      int httpCode = http.POST(payload);
-        //
-        //      if(httpCode > 0) {
-        //          Serial.printf("HTTP POST\nCode: %d\n", httpCode);
-        //
-        //          if(httpCode == HTTP_CODE_OK) {
-        //              Serial.println("Ok");
-        //          }
-        //      } else {
-        //          Serial.printf("Error: %s\n",
-        //          http.errorToString(httpCode).c_str());
-        //      }
-        //
-        //      http.end();
-        delay(pollDelay);
-    } else {
-        Serial.print(".");
-    }
+    
+    Serial.print("L:");
+    Serial.print(smoothedLightLevel);
+    Serial.print(" M:");
+    Serial.println(smoothedMoistureLevel);
+    previousLightLevel = smoothedLightLevel;
+    previousMoistureLevel = smoothedMoistureLevel;
+  } else {
+    Serial.print(".");
+  }
+  delay(pollDelay);
 }
