5de82106d9da25ac711947e2eff3cb7e20c7545e
Julia <jgroza24@gmail.com>
2023-09-16 21:57:56 -0400

    Adding chat bot functions

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,38 +1,85 @@
 import { Box, Button, Container } from "@mui/material"
 import Grid from '@mui/material/Unstable_Grid2';
-import { LineChart } from "@mui/x-charts";
 
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+	ChatContainer,
+	Avatar,
+	Message,
+	MessageInput,
+	MessageList,
+} from "@chatscope/chat-ui-kit-react";
+	
 const Home = () => {
 	return (
 		<Box p={4}>
 			<Container>
 				<Grid container spacing={2}>
-					<Grid xs={4}>
+					<Grid xs={5}>
 						<Box color={"primary.main"} fontSize={64} py={24}>
 							<strong>Some Text.</strong>
 						</Box>
 					</Grid>
-					<Grid xs={1}>
+					{/* <Grid xs={1}>
 						<Box borderRadius={3} bgcolor={"darkgray"} height={"100%"} m={5} minWidth={5}></Box>
-					</Grid>
+					</Grid> */}
 					<Grid xs={7}>
-						<Box pt={12}>
-							<LineChart xAxis={[{ data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }]}
-								series={[{ data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }]}
-								height={400} />
-						</Box>
-						<Box>
-							<Box display="flex" flexDirection="row" justifyContent="space-around">
-								<Button variant="contained">Press Me</Button>
-								<Button variant="outlined">Press Me 2!</Button>
-								<Button variant="outlined">Water plants</Button>
-							</Box>
-						</Box>
+						initialIncomingMessage();
 					</Grid>
 				</Grid>
 			</Container>
 		</Box>
 	)
+}
+
+function outgoingMessage() {
+	<div style={{ position: "relative", height: "500px" }}>
+		<ChatContainer>
+		<MessageList>
+			<Message
+			model = {{
+				message: "Hello my friend",
+				sentTime: "just now",
+				direction: "outgoing",
+				position: "single"
+			}}
+			/>
+		</MessageList>
+		<MessageInput placeholder="Type message here" />
+		</ChatContainer>
+	</div>;
+	}
+
+function incomingMessage() {
+	<div style={{ position: "relative", height: "500px" }}>
+		<ChatContainer>
+		<MessageList>
+			<Message 
+			model = {{
+				message: "Hello my friend",
+				sentTime: "just now",
+				direction: "incoming",
+				position: "single"
+			}}> <Avatar src="" name="Akane"/> </Message>
+		</MessageList>
+		</ChatContainer>
+	</div>;
+}
+
+function initialIncomingMessage() {
+	<div style={{ position: "relative", height: "500px" }}>
+		<ChatContainer>
+		<MessageList>
+			<Message 
+			model = {{
+				message: "Hello my friend",
+				sentTime: "just now",
+				direction: "incoming",
+				position: "single"
+			}}> <Avatar src="" name="Akane"/> </Message>
+		</MessageList>
+		</ChatContainer>
+	</div>;
 }
 
 export default Home
