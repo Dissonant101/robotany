c153240d45f227c9bbe0016194a276d982409244
Julia <jgroza24@gmail.com>
2023-09-17 00:42:12 -0400

    Outgoing messages secured

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,28 +3,52 @@ import Grid from '@mui/material/Unstable_Grid2';
 
 import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
+	MainContainer,
 	ChatContainer,
 	Avatar,
 	Message,
 	MessageInput,
 	MessageList,
 } from "@chatscope/chat-ui-kit-react";
+
+import akaneAvatar from "../assets/akane.svg";
+import { useState } from "react";
 	
 const Home = () => {
+	const [currentText, setCurrentText] = useState("")
+	const [messages, setMessages] = useState([{
+		message: "Hi! Want to check in on your plants?",
+		sentTime: "just now",
+		direction: "incoming"
+	}])
 	return (
 		<Box p={4}>
 			<Container>
 				<Grid container spacing={2}>
 					<Grid xs={5}>
 						<Box color={"primary.main"} fontSize={64} py={24}>
-							<strong>Some Text.</strong>
+							<strong>Talk2Plants.</strong>
 						</Box>
 					</Grid>
-					{/* <Grid xs={1}>
-						<Box borderRadius={3} bgcolor={"darkgray"} height={"100%"} m={5} minWidth={5}></Box>
-					</Grid> */}
 					<Grid xs={7}>
-						initialIncomingMessage();
+						<div style={{ position: "relative", height: "500px" }}>
+						<MainContainer style={{fontSize: "1.3em"}}>
+							<ChatContainer>
+								<MessageList>
+									{messages.map(message => 
+										<Message model = {{
+											message: message.message,
+											sentTime: message.sentTime,
+											direction: message.direction,
+											position: "single"
+										}}/>)}
+								</MessageList>
+								<MessageInput onChange={(_, textContent) => setCurrentText(textContent)}
+									onSend={() => setMessages([...messages, {message: currentText, sentTime: "just now", direction: "outgoing"}])}
+									attachButton={false} placeholder="Type message here" />
+							</ChatContainer>
+						</MainContainer>
+						</div>
 					</Grid>
 				</Grid>
 			</Container>
@@ -32,10 +56,8 @@ const Home = () => {
 	)
 }
 
-function outgoingMessage() {
-	<div style={{ position: "relative", height: "500px" }}>
-		<ChatContainer>
-		<MessageList>
+function OutgoingMessage() {
+	return <div style={{ position: "relative", height: "500px" }}>
 			<Message
 			model = {{
 				message: "Hello my friend",
@@ -44,42 +66,36 @@ function outgoingMessage() {
 				position: "single"
 			}}
 			/>
-		</MessageList>
-		<MessageInput placeholder="Type message here" />
-		</ChatContainer>
 	</div>;
 }
 
-function incomingMessage() {
-	<div style={{ position: "relative", height: "500px" }}>
-		<ChatContainer>
-		<MessageList>
-			<Message 
-			model = {{
-				message: "Hello my friend",
-				sentTime: "just now",
-				direction: "incoming",
-				position: "single"
-			}}> <Avatar src="" name="Akane"/> </Message>
-		</MessageList>
-		</ChatContainer>
+function IncomingMessage() {
+	return <div style={{ position: "relative", height: "500px" }}>
+		<MainContainer>
+			<ChatContainer>
+				<MessageList>
+					<Message 
+					model = {{
+						message: "Hello my friend",
+						sentTime: "just now",
+						direction: "incoming",
+						position: "single"
+					}}> <Avatar src={akaneAvatar} name="Akane"/> </Message>
+				</MessageList>
+				<MessageInput placeholder="Type message here" />
+			</ChatContainer>
+		</MainContainer>
 	</div>;
 }
 
-function initialIncomingMessage() {
-	<div style={{ position: "relative", height: "500px" }}>
-		<ChatContainer>
-		<MessageList>
-			<Message 
+function InitialIncomingMessage() {
+	return <Message 
 			model = {{
-				message: "Hello my friend",
+				message: "Hello",
 				sentTime: "just now",
 				direction: "incoming",
 				position: "single"
-			}}> <Avatar src="" name="Akane"/> </Message>
-		</MessageList>
-		</ChatContainer>
-	</div>;
+			}}> <Avatar src={akaneAvatar} name="Akane"/> </Message>
 }
 
 export default Home
